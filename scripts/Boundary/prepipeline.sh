8e8fa3e6b3b34e73b251ec2f95ecf734b5d5bb2c
blaverty16 <39194013+blaverty16@users.noreply.github.com>
2019-03-21 14:19:41 -0400

    Update prepipeline.sh

--- scripts/Boundary/prepipeline.sh
+++ scripts/Boundary/prepipeline.sh
@@ -1,19 +1,19 @@
 #!/bin/
 
-/usr/local/blast/current/bin/blastn -query ~/sepsis2/AMR4/$1 -out ~/sepsis2/AMR4/BandS/$2 -db nt -qcov_hsp_perc 70 -outfmt "6 qseqid saccver qstart qend sstart send length qcovs staxid ssciname evalue"
-#wait
+/usr/local/blast/current/bin/blastn -query ~/sepsis2/AMR4/$1 -out ~/sepsis2/AMR4/BandS/$2 -db nt -qcov_hsp_perc 100 -evalue 1e-20 -perc_identity 90 -outfmt "6 qseqid saccver qseq sseq sstart send length qcovs staxid ssciname evalue"
+wait
 
-#~/sepsis2/AMR4/rmSyn.py ~/sepsis2/AMR4/BandS/$2 > ~/sepsis2/AMR4/BandS/$2NoSyn
-#wait
-#
-#sort -k 2 ~/sepsis2/AMR4/BandS/$2NoSyn > ~/sepsis2/AMR4/BandS/$2NoSynSort 
-#wait
-#
-#bash ~/sepsis2/AMR4/genContextInputAMRblast.sh ~/sepsis2/AMR4/BandS/$2NoSynSort > ~/sepsis2/AMR4/BandS/AMRstruct$2  
-#wait
+~/sepsis2/AMR4/rmSyn.py ~/sepsis2/AMR4/BandS/$2 > ~/sepsis2/AMR4/BandS/$2NoSyn
+wait
 
-#bash ~/sepsis2/AMR4/insARO.sh ~/sepsis2/AMR4/arolist ~/sepsis2/AMR4/BandS/AMRstruct$2
-#wait
-#
-#~/sepsis2/AMR4/collapseOverlap.pl -s -m -c -t 0.99 ~/sepsis2/AMR4/BandS/AMRstruct$2 > ~/sepsis2/AMR4/BandS/AMRstruct$2col
+sort -k 2 ~/sepsis2/AMR4/BandS/$2NoSyn > ~/sepsis2/AMR4/BandS/$2NoSynSort 
+wait
+
+bash ~/sepsis2/AMR4/genContextInputAMRblast.sh ~/sepsis2/AMR4/BandS/$2NoSynSort > ~/sepsis2/AMR4/BandS/AMRstruct$2  
+wait
+
+bash ~/sepsis2/AMR4/insARO.sh ~/sepsis2/AMR4/arolist ~/sepsis2/AMR4/BandS/AMRstruct$2
+wait
+
+~/sepsis2/AMR4/collapseOverlap.pl -s -m -c -t 0.99 ~/sepsis2/AMR4/BandS/AMRstruct$2 > ~/sepsis2/AMR4/BandS/AMRstruct$2col
 
